9bb844c343c4920ae2d813fb16d1de2234e34ad4
belal-allala <belalallala810@gmail.com>
2025-04-22 22:08:31 +0100

    Importe les classes de modèle dans js/script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+import Livre from './models/Livre.js';
+import Utilisateur from './models/Utilisateur.js';
+import Categorie from './models/Categorie.js';
+import Emprunt from './models/Emprunt.js';
