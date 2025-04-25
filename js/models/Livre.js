4a7fed8435b7219332171ff7ee07439ef5c127a1
belal-allala <belalallala810@gmail.com>
2025-04-25 21:15:38 +0100

    amélioration de la classe Livre avec méthodes statiques et sérialisation

--- js/models/Livre.js
+++ js/models/Livre.js
@@ -1,11 +1,12 @@
-class Livre {
+export default class Livre {
     constructor(id, titre, auteur, datePublication, idCategorie, statut) {
-    this.id = id;
-    this.titre = titre;
-    this.auteur = auteur;
-    this.datePublication = datePublication;
-    this.idCategorie = idCategorie;
-    this.statut = statut; 
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.datePublication = datePublication;
+        this.idCategorie = idCategorie;
+        this.statut = statut; 
+        this.nombreEmprunts = 0;
     }
 
     modifier(titre, auteur, datePublication, idCategorie) {
@@ -20,8 +21,43 @@ class Livre {
     }
  
     supprimer() {
+        console.log(`Suppression du livre ${this.titre}`);
     }
  
     incrementerEmprunts() {
+        this.nombreEmprunts++;
+        return this.nombreEmprunts;
+    }
+    
+    static async getAll() {
+        // Code pour récupérer tous les livres depuis l'API
+    }
+    
+    static async getById(id) {
+        // Code pour récupérer un livre par son ID depuis l'API
+    }
+    
+    static async create(livre) {
+        // Code pour créer un livre via l'API
+    }
+    
+    static async update(id, livre) {
+        // Code pour mettre à jour un livre via l'API
+    }
+    
+    static async delete(id) {
+        // Code pour supprimer un livre via l'API
+    }
+    
+    toJSON() {
+        return {
+            id: this.id,
+            titre: this.titre,
+            auteur: this.auteur,
+            datePublication: this.datePublication,
+            idCategorie: this.idCategorie,
+            statut: this.statut,
+            nombreEmprunts: this.nombreEmprunts
+        };
     }
 }
