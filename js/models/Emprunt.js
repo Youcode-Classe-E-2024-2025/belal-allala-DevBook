d63d0d6958cdd92aa818034236c8471abb0dbd7e
belal-allala <belalallala810@gmail.com>
2025-04-25 21:17:23 +0100

    amélioration de la classe Emprunt avec méthodes statiques

--- js/models/Emprunt.js
+++ js/models/Emprunt.js
@@ -1,12 +1,13 @@
-class Emprunt {
+export default class Emprunt {
     constructor(id, idLivre, idUser, dateEmprunt, dateRetourPrevue, dateRetourEffective = null) {
-    this.id = id;
-    this.idLivre = idLivre;
-    this.idUser = idUser;
-    this.dateEmprunt = dateEmprunt;
-    this.dateRetourPrevue = dateRetourPrevue;
-    this.dateRetourEffective = dateRetourEffective;
+        this.id = id;
+        this.idLivre = idLivre;
+        this.idUser = idUser;
+        this.dateEmprunt = dateEmprunt;
+        this.dateRetourPrevue = dateRetourPrevue;
+        this.dateRetourEffective = dateRetourEffective;
     }
+    
     retourner(dateRetour) {
         this.dateRetourEffective = dateRetour;
     }
@@ -17,5 +18,45 @@ class Emprunt {
 
     estEnRetard() {
         return this.dateRetourEffective === null && new Date() > new Date(this.dateRetourPrevue);
+    }
+    
+    // Méthodes statiques pour la persistance
+    static async getAll() {
+        // Code pour récupérer tous les emprunts depuis l'API
+    }
+    
+    static async getById(id) {
+        // Code pour récupérer un emprunt par son ID depuis l'API
+    }
+    
+    static async getByLivre(idLivre) {
+        // Code pour récupérer les emprunts d'un livre depuis l'API
+    }
+    
+    static async getByUser(idUser) {
+        // Code pour récupérer les emprunts d'un utilisateur depuis l'API
+    }
+    
+    static async create(emprunt) {
+        // Code pour créer un emprunt via l'API
+    }
+    
+    static async update(id, emprunt) {
+        // Code pour mettre à jour un emprunt via l'API
+    }
+    
+    static async delete(id) {
+        // Code pour supprimer un emprunt via l'API
+    }
+    
+    toJSON() {
+        return {
+            id: this.id,
+            idLivre: this.idLivre,
+            idUser: this.idUser,
+            dateEmprunt: this.dateEmprunt,
+            dateRetourPrevue: this.dateRetourPrevue,
+            dateRetourEffective: this.dateRetourEffective
+        };
     }
 }
