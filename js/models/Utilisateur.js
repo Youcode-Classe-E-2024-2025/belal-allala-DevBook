09be8c1adb6ead75779f9541c168750d48e03e8c
belal-allala <belalallala810@gmail.com>
2025-04-25 21:16:42 +0100

    amélioration de la classe Utilisateur avec méthodes statiques et sécurité

--- js/models/Utilisateur.js
+++ js/models/Utilisateur.js
@@ -1,11 +1,12 @@
-class Utilisateur {
+export default class Utilisateur {
     constructor(id, nom, email, password, dateInscription) {
-    this.id = id;
-    this.nom = nom;
-    this.email = email;
-    this.password = password; 
-    this.dateInscription = dateInscription;
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+        this.password = password; 
+        this.dateInscription = dateInscription;
     }
+    
     verifierMotDePasse(password) {
         return this.password === password;
     }
@@ -22,5 +23,35 @@ class Utilisateur {
         }
         return false;
     }
-
+    
+    // Méthodes statiques pour la persistance
+    static async getAll() {
+        // Code pour récupérer tous les utilisateurs depuis l'API
+    }
+    
+    static async getById(id) {
+        // Code pour récupérer un utilisateur par son ID depuis l'API
+    }
+    
+    static async create(utilisateur) {
+        // Code pour créer un utilisateur via l'API
+    }
+    
+    static async update(id, utilisateur) {
+        // Code pour mettre à jour un utilisateur via l'API
+    }
+    
+    static async delete(id) {
+        // Code pour supprimer un utilisateur via l'API
+    }
+    
+    toJSON() {
+        // Ne pas inclure le mot de passe dans l'objet JSON
+        return {
+            id: this.id,
+            nom: this.nom,
+            email: this.email,
+            dateInscription: this.dateInscription
+        };
+    }
 }
